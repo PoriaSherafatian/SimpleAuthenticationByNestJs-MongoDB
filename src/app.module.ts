d78a915dec54a652485d9740906b8ac2b8714e74
Poria Sherafatian <147476035+PoriaSherafatian@users.noreply.github.com>
2024-10-15 18:13:30 +0330

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://root:hddTMwOTA4YsdfOWNZGE1M2NhYTMwOTA4@195.248.240.237:27018/'),
+    MongooseModule.forRoot(),
     AuthModule],
   controllers: [AppController],
   providers: [AppService],
